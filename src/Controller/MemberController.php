30cac1c4fa1f5b06ff3c9e5dc4907374b867385d
Francis Nimpagaritse <franimpa@yahoo.fr>
2022-03-02 14:10:15 +0100

    finances admin pages created

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\MeetingRepository;
+use App\Entity\User;
+use App\Form\UserType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MemberController extends AbstractController
 {
@@ -19,6 +22,29 @@ class MemberController extends AbstractController
 
         return $this->render('member/index.html.twig', [
             'members' => $members,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/members/edit/{id}", name="admin_members_edit")
+     */
+    public function edit(User $member, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(UserType::class, $member);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            $this->addFlash('succes', 'Le membre <strong>' . $member->getLastname() . $member->getFirstname() . '</strong> a été modifié avec succès !');
+
+            return $this->redirectToRoute('admin_members_index');
+
+        }
+
+        return $this->render('member/edit.html.twig', [
+            'form' =>   $form->createView(),
+            'member' => $member,
         ]);
     }
 }
