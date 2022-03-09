eadabb7eda525dda1b63e9deba770b1f5b3e5b27
Francis Nimpagaritse <franimpa@yahoo.fr>
2022-03-09 19:50:32 +0100

    test CI

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -59,6 +59,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     *  @Assert\NotBlank(message="Le numéro de téléphone est obligatoire")
      */
     private $phone;
 
